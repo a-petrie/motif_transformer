618308d042d14fc6a546765bff4b5a02c174f7eb
a-petrie <allantpetrie@googlemail.com>
2022-07-28 16:22:44 +0100

    Adding additional test cases.
    Also refactoring tests file to use test parametrization, thus avoiding
    unescessary duplication.

--- test.py
+++ test.py
@@ -3,45 +3,26 @@ from test_utils import *
 from motif import Motif
 from transform import transform
 
+import pytest
+
 # selection functions used as mocks here
 def select_last_note(motif: Motif) -> int:
     return motif.non_rest_indices()[-1]
 
 
 def select_first_note(motif: Motif) -> int:
-    return 0
+    return motif.non_rest_indices()[0]
 
+test_data = [
+        (mary_had_a_little_lamb(), mary_had_a_little_lamb_with_last_note_as_rest(), select_last_note),
+        (mary_had_a_little_lamb(), mary_had_a_little_lamb_with_first_note_as_rest(), select_first_note),
+        (mary_had_a_little_lamb_with_rest_between_notes(), mary_had_a_little_lamb_with_last_note_as_rest_and_rest_between_notes(), select_last_note),
+        (mary_had_a_little_lamb_with_rest_between_notes(), mary_had_a_little_lamb_with_first_note_as_rest_and_rest_between_notes(), select_first_note),
+        (mary_had_a_little_lamb_off_beats(), mary_had_a_little_lamb_off_beats_and_last_note_as_rest(), select_last_note),
+        (mary_had_a_little_lamb_off_beats(), mary_had_a_little_lamb_off_beats_and_first_note_as_rest(), select_first_note),
+    ]
 
-def test_last_note_deletion():
-    motif = mary_had_a_little_lamb()
-    expected_motif = mary_had_a_little_lamb_with_last_note_as_rest()
-
-    transformed_motif = transform(motif, select_last_note)
-    assert expected_motif.notes == transformed_motif.notes
-
-
-def test_first_note_deletion():
-    motif = mary_had_a_little_lamb()
-    expected_motif = mary_had_a_little_lamb_with_first_note_as_rest()
-
-    transformed_motif = transform(motif, select_first_note)
-    assert expected_motif.notes == transformed_motif.notes
-
-
-def test_last_note_deletion_with_rests():
-    motif = mary_had_a_little_lamb_with_rest_between_notes()
-    expected_motif = mary_had_a_little_lamb_with_last_note_as_rest_and_rest_between_notes()
-
-    transformed_motif = transform(motif, select_last_note)
-    assert expected_motif.notes == transformed_motif.notes
-
-
-def test_first_note_deletion_with_rests():
-    motif = mary_had_a_little_lamb_with_rest_between_notes()
-    expected_motif = mary_had_a_little_lamb_with_first_note_as_rest_and_rest_between_notes()
-
-    transformed_motif = transform(motif, select_first_note)
-    assert expected_motif.notes == transformed_motif.notes
-
-
-
+@pytest.mark.parametrize("initial,expected,note_selector", test_data)
+def test_motif_deletions(initial: Motif, expected: Motif, note_selector: callable) -> None:
+    transformed = transform(initial, note_selector)
+    assert transformed.notes == expected.notes
--- test_utils.py
+++ test_utils.py
@@ -97,3 +97,58 @@ def mary_had_a_little_lamb_with_first_note_as_rest_and_rest_between_notes() -> M
     return Motif(notes)
 
 
+def mary_had_a_little_lamb_off_beats() -> Motif:
+    notes = [
+        Note('rest', 0.5),
+        Note('B', 0.5),
+        Note('rest', 0.5),
+        Note('G', 0.5),
+        Note('rest', 0.5),
+        Note('A', 0.5),
+        Note('rest', 0.5),
+        Note('G', 0.5),
+        Note('rest', 0.5),
+        Note('B', 0.5),
+        Note('rest', 0.5),
+        Note('B', 0.5),
+        Note('rest', 0.5),
+        Note('B', 1.5),
+    ]
+    return Motif(notes)
+
+
+def mary_had_a_little_lamb_off_beats_and_last_note_as_rest() -> Motif:
+    notes = [
+        Note('rest', 0.5),
+        Note('B', 0.5),
+        Note('rest', 0.5),
+        Note('G', 0.5),
+        Note('rest', 0.5),
+        Note('A', 0.5),
+        Note('rest', 0.5),
+        Note('G', 0.5),
+        Note('rest', 0.5),
+        Note('B', 0.5),
+        Note('rest', 0.5),
+        Note('B', 0.5),
+        Note('rest', 2.),
+    ]
+    return Motif(notes)
+
+
+def mary_had_a_little_lamb_off_beats_and_first_note_as_rest() -> Motif:
+    notes = [
+        Note('rest', 1.5),
+        Note('G', 0.5),
+        Note('rest', 0.5),
+        Note('A', 0.5),
+        Note('rest', 0.5),
+        Note('G', 0.5),
+        Note('rest', 0.5),
+        Note('B', 0.5),
+        Note('rest', 0.5),
+        Note('B', 0.5),
+        Note('rest', 0.5),
+        Note('B', 1.5),
+    ]
+    return Motif(notes)
