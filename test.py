03889e7d889a031aafae6a4308abacdf09df209a
a-petrie <allantpetrie@googlemail.com>
2022-07-28 15:45:10 +0100

    Adding test case for rests and first note deletion
    Again, this came "for free" from implementation of previous test case so
    no additional code was meeded.

--- test.py
+++ test.py
@@ -3,17 +3,6 @@ from test_utils import *
 from motif import Motif
 from transform import transform
 
-'''
-    Need to figure out how note selection works within motifs.
-    - Notes will likely be indexed, so could return index
-        - This seems like a sound enough abstraction
-        - But there's coupling issues
-        - This indexing seems like an implementation detail, probably don't want to see it in our test
-        - Actually, this may be fine, it's a mock to get the ball rolling
-        - Also doesn't have to be perfect first time
-'''
-
-
 # selection functions used as mocks here
 def select_last_note(motif: Motif) -> int:
     return motif.non_rest_indices()[-1]
@@ -53,4 +42,6 @@ def test_first_note_deletion_with_rests():
 
     transformed_motif = transform(motif, select_first_note)
     assert expected_motif.notes == transformed_motif.notes
+
+
 
